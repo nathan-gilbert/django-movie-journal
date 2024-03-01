728214b6f9fe645881408b4bd803940728039652
Nathan Gilbert <nathan.gilbert@gmail.com>
2024-02-29 22:53:54 -0700

    Add procfile release

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn --pythonpath mysite mysite.wsgi:application
+release: ./manage.py migrate --no-input
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,14 +76,7 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
+DATABASES = {"default": dj_database_url.config(conn_max_age=600, ssl_require=True)}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
